2fa6d8be8d57464558283c329acdb4c14836b946
Gaurav Chikhale <gauravchl@users.noreply.github.com>
2017-09-28 14:06:52 +0530

    Add animation

--- src/index.jsx
+++ src/index.jsx
@@ -4,19 +4,38 @@ function ReactBellIcon(props) {
 
   let { width, height } = props;
 
-
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <svg width='140' height='140' viewBox='0 0 20 20' >
-          <g id='Page-1' stroke='none' strokeWidth='1' fill='none' fillRule='evenodd'>
-              <g id='bell' transform='translate(-2.000000, -2.000000)'>
-                <path d='M19.97,10.5 L21.97,10.5 C21.82,7.3 20.24,4.48 17.85,2.65 L16.43,4.08 C18.45,5.53 19.82,7.85 19.97,10.5 Z' id='Path' fill='#000000'></path>
-                <path d='M7.58,4.08 L6.15,2.65 C3.75,4.48 2.17,7.3 2.03,10.5 L4.03,10.5 C4.18,7.85 5.54,5.53 7.58,4.08 L7.58,4.08 Z' id='Path' fill='#000000'></path>
-                <path d='M12,22 C12.14,22 12.27,21.99 12.4,21.96 C13.05,21.82 13.58,21.38 13.84,20.78 C13.94,20.54 13.99,20.28 13.99,20 L9.99,20 C10,21.1 10.89,22 12,22 Z' id='Path' fill='#000000'></path>
-                <path d='M18,11 C18,7.93 16.36,5.36 13.5,4.68 L13.5,4 C13.5,3.17 12.83,2.5 12,2.5 C11.17,2.5 10.5,3.17 10.5,4 L10.5,4.68 C7.63,5.36 6,7.92 6,11 L6,16 L4,18 L4,19 L20,19 L20,18 L18,16 L18,11 Z' id='Shape' fill='#000000' fillRule='nonzero'></path>
-                <polygon id='Shape' points='0 0 24 0 24 24 0 24'></polygon>
-              </g>
+      <svg width='140' height='140' viewBox='0 0 20 20' style={{ overflow:'initial' }} >
+        <g id='Page-1' stroke='none' strokeWidth='1' fill='none' fillRule='evenodd'>
+          <g id='bell' fill='#000000'>
+            <animateTransform
+              attributeName='transform'
+              attributeType='XML'
+              type='rotate'
+              keyTimes='0; 0.5; 1'
+              values='-12 10 4; 12 10 4; -12 10 4;'
+              dur='.2s'
+              begin='0s'
+              repeatCount='indefinite'
+            />
+          <path d='M17.97,8.5 L19.97,8.5 C19.82,5.3 18.24,2.48 15.85,0.65 L14.43,2.08 C16.45,3.53 17.82,5.85 17.97,8.5 Z' id='Path'></path>
+              <path d='M5.58,2.08 L4.15,0.65 C1.75,2.48 0.17,5.3 0.03,8.5 L2.03,8.5 C2.18,5.85 3.54,3.53 5.58,2.08 L5.58,2.08 Z' id='Path'></path>
+              <path d='M10,20 C10.14,20 10.27,19.99 10.4,19.96 C11.05,19.82 11.58,19.38 11.84,18.78 C11.94,18.54 11.99,18.28 11.99,18 L7.99,18 C8,19.1 8.89,20 10,20 Z' id='Path'>
+                <animateTransform
+                  attributeName='transform'
+                  attributeType='XML'
+                  type='translate'
+                  keyTimes='0; 0.5; 1'
+                  values='-8 0; 8 0; -8 0;'
+                  dur='.3s'
+                  begin='0s'
+                  repeatCount='indefinite'
+                />
+              </path>
+              <path d='M16,9 C16,5.93 14.36,3.36 11.5,2.68 L11.5,2 C11.5,1.17 10.83,0.5 10,0.5 C9.17,0.5 8.5,1.17 8.5,2 L8.5,2.68 C5.63,3.36 4,5.92 4,9 L4,14 L2,16 L2,17 L18,17 L18,16 L16,14 L16,9 Z' id='Shape' fillRule='nonzero'></path>
           </g>
+        </g>
       </svg>
     </div>
   )
